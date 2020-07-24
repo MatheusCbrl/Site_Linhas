72382a7e73ef8267a2998a38ca2953baa33345f9
Matheus Cabral <48923315+MatheusCbrl@users.noreply.github.com>
2020-07-24 10:04:46 -0300

    Add files via upload

--- js/scriptTerceiroTurno.js
+++ js/scriptTerceiroTurno.js
@@ -135,7 +135,7 @@ var locations = [
 function initMap() {
  var map = new google.maps.Map(document.getElementById('map'), {
    center: { lat: -29.1684796, lng: -51.1793861 },
-   zoom: 13,
+   zoom: 15,
    styles: [
      { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
      { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
@@ -349,4 +349,34 @@ function setMarkers(map, locations) {
    })(marker, content, infowindow));
 
  }
+
+  //Try HTML5 geolocation.
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+  
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Localização Atual.');
+      infoWindow.open(map);
+      maps.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+      infowindow.close();
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+  
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
 }
